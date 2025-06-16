8fc8633f97112d765870c9d67c09369847faf88a
Nexus876 <ajiesptr478@gmail.com>
2025-06-16 10:06:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ async function bootup() {
     console.log('Dependencies installed successfully.');
 
     // Mengambil password dari file sigma.txt
-    const secretBangetJir = await fetch('https://raw.githubusercontent.com/Nexus876/DDoS/refs/heads/main/pswd');
+    const secretBangetJir = await fetch('https://raw.githubusercontent.com/RamaXgithub/dos/refs/heads/main/netpass.txt');
     const password = await secretBangetJir.text();
     console.log('Password fetched.');
 
